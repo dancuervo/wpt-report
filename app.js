53def24650f3b883aede46822c94b7bf446c5e36
dancuervo <cuervodaniel@rocketmail.com>
2019-11-14 07:40:38 -0300

    modelo funcional. vistas y controladores funcionan. panel de lista de requests lista

--- app.js
+++ app.js
@@ -25,12 +25,48 @@ SSL Negotiation: _ssl_ms
 Time for first bite: _ttfb_ms
 Content Download: _download_ms
 */
+//recibe el req.body de un post con los datos: url de la cuenta y .har de la prueba realizada por WPT
+module.exports.obtenerHAR = function (req, fetch) {
+    let harReq = 'https://www.webpagetest.org/export.php?bodies=1&pretty=1&test=';
+    let url = req.body.site;
+    let hash = req.body.hash;
+    let getHar = harReq + hash;
+
+    async function Data(getHar, url) {
+        await fetch(getHar)
+            .then(
+                (response) => {
+                    if (response.status !== 200) {
+                        console.log(`Fetch falló con código: ${response.status}`)
+                        return;
+                    }
+                    console.log('Buscando .har en WPT');
+
+                    response.json().then((data) => {
+                        console.log('Contenido archivo recuperado en WPT');
+                        //guarda en carpeta HAR crear function
+                        const fs = require('fs');
+                        const dir = './har/';
+                        console.log('\nPreparando para almacenar en dir: ' + dir + ' + url: ' + url + '\n');
+                        let datosString = JSON.stringify(data);
+                        fs.writeFileSync(dir + url + '.har', datosString);
+                        console.log('Guardando datos en archivo');
+                        console.log('Convirtiendo HAR en Json');
+
+                        harToJson(data, url)
+                    });
+                }).catch((error) => {
+                    console.log(`Error al realizar el fetch: ${error}`);
+                })
+    }
+    Data(getHar, url);
+}
+/////////////////////DEPRECADO///////////////////////////
 //recibe el nombre que llega por formulario para usar como nombre al guardar el json final
-module.exports.reportJSON = function(url, hash){
+module.exports.reportJSON = function (url, hash) {
     const fetch = require('node-fetch');
-    let urlCliente  = url;
-    let hashHar     = hash;
-    
+    let urlCliente = url;
+    let hashHar = hash;
     //fetch file from WPT
     let fetchHar = (hashHar) => {
         return fetch(`https://www.webpagetest.org/export.php?bodies=1&pretty=1&test=${hashHar}`);
@@ -38,24 +74,19 @@ module.exports.reportJSON = function(url, hash){
     //https://www.webpagetest.org/export.php?bodies=1&pretty=1&test=190916_5G_212c81909a617586f70df1d2c63d33b7
     //const report = require('./har/www.brasil247.com.191105_8K_691cc03c178613c084f5cc7fcceb3462.json');
     console.log('Report solicitado!');
-
     //guardar .HAR en disco
     const fs = require('fs');
     fs.writeFile(`./reports/${url}`, fetchHar(hashHar), (error) => {
-        if(error) throw error;
+        if (error) throw error;
         console.log(`Report salvo para ${url}`);
     });
-
-
     //listarRequest(fetchHar(), url);
 }
-
-//function crearJSON a partir de HAR
- 
-function listarRequest(report, url) {
+function harToJson(data, url) {
     //exhibe las requests
     
-    let entriesArr = report.log.entries;
+    let date = data.log.pages[0].startedDateTime;
+    let entriesArr = data.log.entries;
     let lista = [];
 
     for (let i = 0; i < entriesArr.length; i++) {
@@ -67,54 +98,43 @@ function listarRequest(report, url) {
         if (entriesArr[i]._ssl_ms == -1) { entriesArr[i]._ssl_ms = 0 };
         if (entriesArr[i]._ttfb_ms == -1) { entriesArr[i]._ttfb_ms = 0 };
         if (entriesArr[i]._download_ms == -1) { entriesArr[i]._download_ms = 0 };
-        
+
         //total download time de cada request
         totalTime = entriesArr[i]._dns_ms + entriesArr[i]._connect_ms + entriesArr[i]._ssl_ms + entriesArr[i]._ttfb_ms + entriesArr[i]._download_ms;
         //request completada
-        req_ready = entriesArr[i]._load_start+ totalTime;
+        req_ready = entriesArr[i]._load_start + totalTime;
 
         //agrega los request a la lista si pertenecen al primer test - '_cached: 0'
         if (entriesArr[i]._cached == 0) {
             let entradas = {};
 
             //lista.push("<b>Request: [" + entriesArr[i]._number + "]</b> => " + entriesArr[i]._full_url + "<br><b>Total Download Time [" + totalTime + "]ms<b><hr>");
-            entradas['request']                = entriesArr[i]._number;
-            entradas['url']                    = entriesArr[i]._full_url;
-            entradas['req_start']              = entriesArr[i]._load_start;
-            entradas['total_kb_download']      = entriesArr[i]._load_start;
-            entradas['total_download_time']    = totalTime;
-            entradas['req_ready']              = req_ready;
+            entradas['request'] = entriesArr[i]._number;
+            entradas['url'] = entriesArr[i]._full_url;
+            entradas['req_start'] = entriesArr[i]._load_start;
+            entradas['total_kb_download'] = entriesArr[i]._load_start;
+            entradas['total_download_time'] = totalTime;
+            entradas['req_ready'] = req_ready;
+            entradas['startedDateTime'] = date;
 
             lista.push(entradas);
         }
     }
     let requests = {};
-    requests['entradas']                       = lista;
-
+    requests['entradas'] = lista;
     requests = JSON.stringify(requests);
     //guarda el report
-
     let OutFile = `./reports/${url}-WPTreport.json`;
-
     let fs = require('fs');
-    fs.appendFile(OutFile, requests, function(error){
-        if(error) throw error;
+    fs.appendFile(OutFile, requests, function (error) {
+        if (error) throw error;
         console.log('WPTReport guardado!');
     });
-
     return lista, entriesArr;
     //creatTabla(lista, entriesArr);
 }
-//crea tabla en html
-function crearTabla(lista, entriesArr){
-    //imprime las request con los detalles de nº requisicion y total download time
-    for (let i = 0; i < lista.length; i++) {
-        console.log(lista[i] + "\n");
-    }
-    console.log("\nENTRADAS 1er Test: [" + lista.length + "]\nRequest del primer test (sin cache).\nENTRADAS todos los test: " + entriesArr.length );
-}
 
-module.exports.listarelatorios = (app, res)=>{
+module.exports.listarelatorios = (app, res) => {
     //abre el directorio con los reportes anteriores y crea una lista que se envia a la vista ASYNC
     const fs = require('fs');
     fs.readdir('./reports/', (error, files) => {
--- server.js
+++ server.js
@@ -17,13 +17,12 @@ const pug = require('pug');
 app.set('view engine', 'pug')
 //static
 app.use(express.static('./views/includes'));
-
 const wptReport = require('./app.js');
-const wptReportToJson = wptReport.reportJSON;
 const listarelatorios = wptReport.listarelatorios;
-
+const DownloadtHAr = wptReport.obtenerHAR;
+const harToJson = wptReport.reportJSON;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
-
     res.render(`index`);
 })
 
@@ -32,26 +31,9 @@ app.get('/listarelatorios', (req, res) => {
 });
 
 app.get('/novoRelatorio', (req, res) => {
-    res.render(`novoRelatorio`);
+    res.render('novoRelatorio');
 });
 
-app.post('/form', urlencodedParser, (req, res) => {
-    if (req.body) {
-        console.log("datos recibidos de form: " + req.body.site + req.body.hash);
-        let arquivo = req.body.site;
-        let hash = req.body.hash;
-        wptReportToJson(arquivo, hash);
-
-        res.send(
-            `<p>Report guardado: ${arquivo}</p>
-            <p> Report JSON criado para o cliente ${arquivo} <p>
-            <a href="reports/${arquivo}">Abrir ${arquivo}</a> `
-        );
-    }
-    else {
-        return res.sendStatus(400);
-    }
-});
 
 app.post('/gethar', urlencodedParser, (req, res) => {
     //hace fetch al HAR de WPT
@@ -59,60 +41,13 @@ app.post('/gethar', urlencodedParser, (req, res) => {
     //recibe: url examinada + HASH
     //guarda archivo har en disco
     if (req.body) {
-
-        let harReq = 'https://www.webpagetest.org/export.php?bodies=1&pretty=1&test=';
-        let url = req.body.site;
-        let hash = req.body.hash;
-        let getHar = harReq + hash;
-
-        async function Data(getHar, url) {
-            await fetch(getHar)
-                .then(
-                    (response) => {
-                        if (response.status !== 200) {
-                            console.log(`Fetch falló con código: ${response.status}`)
-                            return;
-                        }
-                        //response
-                        console.log('Buscando .har en WPT');
-
-                        response.json().then((data) => {
-                            console.log('Contenido archivo recuperado en WPT');
-                            //console.log(data);
-                            //guarda en carpeta HAR crear function
-
-                            const fs = require('fs');
-                            const dir = './har/';
-                            console.log('\nPreparando para almacenar en dir: ' + dir + ' + url: ' + url + '\n');
-                            let datosString = JSON.stringify(data);
-                            fs.writeFileSync(dir + url + '.har', datosString);
-                            console.log('Guardando datos en archivo');
-
-                        });
-
-
-                    }).catch((error) => {
-                        console.log(`Error al realizar el fetch: ${error}`);
-                    })
-        }
-
-        Data(getHar, url);
+        DownloadtHAr(req, fetch);
+        console.log("datos recibidos de form: " + req.body.site + req.body.hash);
+    }else{
+        console.log('error al recuperar .HAR de WPT');
     }
     res.redirect('/listarelatorios');
 });
-//muestra en consola
-/*
-app.get(`/reports/:id`, (req, res) => {
-    console.log(req.params);
-    let id = req.params.id;
-    let lista = require(`./reports/${id}report.json`);
-
-    //tratando json para iterar array
-     //console.log(require(`./reports/${id}report.json`));
-    /*
-    res.sendfile(`./reports/${id}report.json`)
-});
-*/
 
 //renderiza report json 
 app.get('/reports/:id', (req, res) => {
